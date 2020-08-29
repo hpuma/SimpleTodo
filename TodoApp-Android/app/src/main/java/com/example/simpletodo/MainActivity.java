668d494166db1212109b99b5cdb1b1a44194669d
Henry <37905046+hpuma@users.noreply.github.com>
2020-08-29 04:53:14 -0400

    Saving progress

--- TodoApp-Android/app/src/main/java/com/example/simpletodo/MainActivity.java
+++ TodoApp-Android/app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.simpletodo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    List<String> items;
+
+    Button btnAdd;
+    EditText etItem;
+    RecyclerView rvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnAdd = findViewById(R.id.btnAdd);
+        etItem = findViewById(R.id.etItem);
+        rvItems = findViewById(R.id.rvItems);
+
+
+
+        items = new ArrayList<>();
+        items.add("Buy Something");
+        items.add("Go to the Gym");
+        items.add("Have Fun!");
     }
 }
