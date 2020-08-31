85b4826d30ce4286cc5611aa3af260ca8471a101
Henry <37905046+hpuma@users.noreply.github.com>
2020-08-31 04:08:15 -0400

    Added remove/add functionality and made app read from txt for dummy data

--- TodoApp-Android/app/src/main/java/com/example/simpletodo/ItemsAdapter.java
+++ TodoApp-Android/app/src/main/java/com/example/simpletodo/ItemsAdapter.java
@@ -10,11 +10,16 @@ import java.util.List;
 
 // Displays data from the model into a row
 public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder> {
+  public interface OnLongClickListener {
+    void onItemLongClicked(int position);
+  }
 
   List<String> items;
+  OnLongClickListener longClickListener;
 
-  public ItemsAdapter(List<String> items) {
+  public ItemsAdapter(List<String> items, OnLongClickListener longClickListener) {
     this.items = items;
+    this.longClickListener = longClickListener;
   }
   @NonNull
   @Override// Creates each view
@@ -37,19 +42,26 @@ public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder> 
     return items.size();
   }
 
-
   // Provides easy access to views that represents each row of the list
   class ViewHolder extends RecyclerView.ViewHolder {
-      TextView tvItem;
+
+    TextView tvItem;
 
     public ViewHolder(@NonNull View itemView) {
       super(itemView);
       tvItem = itemView.findViewById(android.R.id.text1);
     }
-
     // Updates the view inside of the view holder with the String item data
     public void bind(String item) {
       tvItem.setText(item);
+      tvItem.setOnLongClickListener(new View.OnLongClickListener() {
+        @Override
+        public boolean onLongClick(View view) {
+          // Notify the listener which position was long pressed
+          longClickListener.onItemLongClicked(getAdapterPosition());
+          return true;
+        }
+      });
     }
   }
 }
--- TodoApp-Android/app/src/main/java/com/example/simpletodo/MainActivity.java
+++ TodoApp-Android/app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -3,11 +3,18 @@ package com.example.simpletodo;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import org.apache.commons.io.FileUtils;
+import java.nio.charset.Charset;
 import android.widget.EditText;
 import android.widget.Button;
+import android.widget.Toast;
 import java.util.ArrayList;
+import java.io.IOException;
+import android.view.View;
 import android.os.Bundle;
+import android.util.Log;
 import java.util.List;
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
   // Data
@@ -17,24 +24,71 @@ public class MainActivity extends AppCompatActivity {
   Button btnAdd;
   EditText etItem;
   RecyclerView rvItems;
-
+  ItemsAdapter itemsAdapter;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
     btnAdd = findViewById(R.id.btnAdd);
     etItem = findViewById(R.id.etItem);
     rvItems = findViewById(R.id.rvItems);
 
     // Creating data
-    items = new ArrayList<>();
-    items.add("Buy Something");
-    items.add("Go to the Gym");
-    items.add("Have Fun!");
+    loadItems(); // Getting items from data file
 
     // Setting data
-    ItemsAdapter itemsAdapter = new ItemsAdapter(items);
+    ItemsAdapter.OnLongClickListener onLongClickListener = new ItemsAdapter.OnLongClickListener() {
+      @Override
+      public void onItemLongClicked(int position) {
+        // Delete the item from the model
+        items.remove(position);
+        // Notify the adapter
+        itemsAdapter.notifyItemRemoved(position);
+        Toast.makeText(getApplicationContext(), "Item was added", Toast.LENGTH_SHORT).show();
+        saveItems();
+      }
+    };
+
+    itemsAdapter = new ItemsAdapter(items, onLongClickListener);
     rvItems.setAdapter(itemsAdapter);
     rvItems.setLayoutManager(new LinearLayoutManager(this));
+
+    btnAdd.setOnClickListener(new View.OnClickListener() {
+      // Adding todo logic
+      @Override
+      public void onClick(View view) {
+        String todoItem = etItem.getText().toString();
+        // Add item to the model
+        items.add(todoItem);
+        // Notify adapter that an item is inserted
+        itemsAdapter.notifyItemInserted(items.size()-1);
+        etItem.setText("");
+        // Notification after todo is added
+        Toast.makeText(getApplicationContext(), "Item was added", Toast.LENGTH_SHORT).show();
+        saveItems();
+      }
+    });
+  }
+  // Managing Data
+  private File getDataFile() {
+    return new File(getFilesDir(), "data.txt");
+  }
+  // Load items by reading every line of the data file
+  private void loadItems() {
+    try {
+      items = new ArrayList<>(FileUtils.readLines(getDataFile(), Charset.defaultCharset()));
+    } catch (IOException e) {
+      items = new ArrayList<>();
+      Log.e("MainActivity", "Error reading items", e);
+    }
+  }
+  // Saves the items by writing them into the data file
+  private  void saveItems() {
+    try {
+      FileUtils.writeLines(getDataFile(), items);
+    } catch (IOException e) {
+      Log.e("MainActivity", "Error with writing items", e);
+    }
   }
 }
